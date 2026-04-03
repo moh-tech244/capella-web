3c44bc24a87827fc7bb97defc1aa208891ddfc91
moh-tech244 <mohamedelm0658@gmail.com>
2026-04-03 21:25:30 +0100

    Create FloatingSupport.tsx

---
+++ src/components/FloatingSupport.tsx
@@ -0,0 +1,8 @@
+export const FloatingSupport = () => {
+  return (
+    <div className="fixed bottom-4 right-4 bg-blue-600 text-white p-3 rounded-full shadow-lg cursor-pointer">
+      Besoin d'aide ?
+    </div>
+  );
+};
+export default FloatingSupport;
