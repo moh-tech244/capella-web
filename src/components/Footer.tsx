9fee202d73f882426cac0c49561639c4698e0c05
moh-tech244 <mohamedelm0658@gmail.com>
2026-04-03 21:23:00 +0100

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="py-6 border-t text-center bg-white">
+      <p className="text-sm text-gray-500">© 2026 Capella Home. Tous droits réservés.</p>
+    </footer>
+  );
+};
+
+export default Footer;
