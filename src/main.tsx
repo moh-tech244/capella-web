ca14158e325f85a11c1b577c72677a5679d6a72e
moh-tech244 <mohamedelm0658@gmail.com>
2026-04-03 23:24:15 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css"; // Injection du style optimisé
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const rootElement = document.getElementById("root");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
