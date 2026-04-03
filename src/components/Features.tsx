6af0c290f495c27ac93b66bd490d371725115151
moh-tech244 <mohamedelm0658@gmail.com>
2026-04-03 21:17:52 +0100

    Create Features.tsx

---
+++ src/components/Features.tsx
@@ -0,0 +1,9 @@
+export const Features = () => {
+  return (
+    <section className="py-10 bg-white text-center">
+      <h2 className="text-2xl font-bold">Nos Services</h2>
+      <p>Optimisation SEO Local & Google Maps.</p>
+    </section>
+  );
+};
+export default Features;
